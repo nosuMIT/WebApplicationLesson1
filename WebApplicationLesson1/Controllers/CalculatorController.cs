c7031e1f8501758741b38549e1bad4a5cfde771a
George Gussaov <gussaov04@gmail.com>
2024-02-29 20:10:20 +0300

    Даже примечание сделал

--- WebApplicationLesson1/Controllers/CalculatorController.cs
+++ WebApplicationLesson1/Controllers/CalculatorController.cs
@@ -4,8 +4,13 @@ namespace WebApplicationLesson1.Controllers
 {
     public class CalculatorController : Controller
     {
-        public string Index(double num1, double num2)
+        public string Index(double num1, double num2, string operation)
         {
+
+            if (operation == "*") return $"{num1} * {num2} = {num1 * num2}";
+            else if (operation == "-") return $"{num1} - {num2} = {num1 - num2}";
+            else if (operation == "+") return $"{num1} + {num2} = {num1 + num2}";
+            else if (operation != null) return "Пожалуйста, введите одну из операций: *, -, +. Либо нее вводите операцию, чтобы получить сумму чисел.";
             return $"{num1} + {num2} = {num1 + num2}";
         }
     }
