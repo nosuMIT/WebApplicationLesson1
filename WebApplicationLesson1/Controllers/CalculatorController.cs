91f3a30a212787660d9c615a4d5a6dab09da8684
George Gussaov <gussaov04@gmail.com>
2024-02-29 19:35:43 +0300

    Калькулятор готов

---
+++ WebApplicationLesson1/Controllers/CalculatorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationLesson1.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public string Index(double num1, double num2)
+        {
+            return $"{num1} + {num2} = {num1 + num2}";
+        }
+    }
+}
