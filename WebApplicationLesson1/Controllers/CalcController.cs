12ef974b2538e44c1d409e18d536177837e5f292
George Gussaov <gussaov04@gmail.com>
2024-02-29 20:34:42 +0300

    готово

---
+++ WebApplicationLesson1/Controllers/CalcController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationLesson1.Controllers
+{
+    public class CalcController : Controller
+    {
+        public string Index(double a, double b, string c)
+        {
+            if (c == "*") return $"{a} * {b} = {a * b}";
+            else if (c == "-") return $"{a} - {b} = {a - b}";
+            else if (c == "+") return $"{a} + {b} = {a + b}";
+            else if (c == "/") return $"{a} / {b} = {a / b}";
+            else if (c != null) return "Пожалуйста, введите одну из операций: *, -, +. Либо нее вводите операцию, чтобы получить сумму чисел.";
+            return $"{a} + {b} = {a + b}";
+        }
+    }
+}
