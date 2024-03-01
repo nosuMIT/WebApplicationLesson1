5de3e0063976c089856dd8e9acc266e9b7d7525d
OlegEKX <olegjarl@gmail.com>
2024-03-01 17:42:31 +0300

    Калькулятор с вариативностью оераций готов

---
+++ WebApplicationLesson1/Controllers/CalculatorController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplicationLesson1.Models;
+
+namespace WebApplicationLesson1.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public string Index(int a, int b, char c)
+        {
+            if (c == '+')
+            {
+                return $"{a} + {b} = {a + b}";
+            }
+            if (c == '*')
+            {
+                return $"{a} * {b} = {a * b}";
+            }
+            if (c == '-')
+            {
+                return $"{a} - {b} = {a - b}";
+            }
+            if (c == '\0')
+            {
+                return $"{a} + {b} = {a + b}";
+            }
+            return $"0 + 0 = 0";
+        }
+    }
+}
