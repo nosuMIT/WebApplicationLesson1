e525363d1388640d49472a88337d94bbf7eef731
GioevArsen <gioev.arsen15@list.ru>
2024-03-04 19:33:48 +0300

    Created StartController with Hello action method which returns appropriate greeting

---
+++ WebApplicationLesson1/Controllers/startController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationLesson1.Controllers
+{
+    public class StartController : Controller
+    {
+        public string Hello()
+        {
+            int currentHour = DateTime.Now.Hour;
+            if (currentHour < 6) return "Доброй ночи!";
+            else if(currentHour < 12) return "Доброе утро!";
+            else if(currentHour < 18) return "Добрый день!";
+            else return "Добрый вечер!";
+        }
+    }
+}
