a8f14c909a5eb1f9f405b5dbbda47d42cfb3fd00
GioevArsen <gioev.arsen15@list.ru>
2024-03-04 20:33:05 +0300

    Created CalculatorContoller, changed Index action method within it, changed endpoint pattern

---
+++ WebApplicationLesson1/Controllers/CalculatorController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationLesson1.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public string Index(double a, double b, char operation)
+        {
+            char[] appropriateOperations = { '-', '+', '*' };
+            switch (operation)
+            {
+                case '-':
+                    return $"{a} {operation} {b} = {a - b}";
+                case '*':
+                    return $"{a} {operation} {b} = {a * b}";
+                default:
+                    if(operation != '+' && operation != 0) return "Пока поддерживаются только операции сложения, вычитания и умножения";
+                    return $"{a} + {b} = {a + b}";
+            }
+        }
+    }
+}
