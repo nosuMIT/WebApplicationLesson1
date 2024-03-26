6e2cb283d60eebf41e52b6a7c5af9f9f34caab37
albor09 <alborov011@gmail.com>
2024-03-26 21:43:10 +0300

    Added hello controller

---
+++ WebApplicationLesson1/Controllers/StartController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace WebApplicationLesson1.Controllers
+{
+    public class StartController : Controller
+    {
+        public string Hello()
+        {
+            if (DateTime.Now.Hour < 6)
+                return "Доброй ночи!";
+            if (DateTime.Now.Hour < 12)
+                return "Доброе утро!";
+            if (DateTime.Now.Hour < 18)
+                return "Добрый день!";
+        
+            return "Добрый вечер!";
+        }
+    }
+}
