a184771e06edc8c025281de6f10fc5fe334d1345
albor09 <alborov011@gmail.com>
2024-03-26 22:17:10 +0300

    Added calculator controller

---
+++ WebApplicationLesson1/Controllers/CalculatorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationLesson1.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public string Index(int a, int b)
+        {
+            return $"{a} + {b} = {a + b}";
+        }
+    }
+}
